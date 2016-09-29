5688072578c0c4adbbf5c5d8097d82f8dfdd0363
sfayman <Sergey_Fayman@bmc.com>
2016-09-29 22:13:17 +0300

    develop temperature module

--- raspberry/gmail_utils.py
+++ raspberry/gmail_utils.py
@@ -1,16 +1,26 @@
-import smtplib
+import smtplib, sys
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-def sendEmail(fromAddr, toAddrs, key, text, skipSend):
+def sendEmail(fromAddr, toAddrs, key, body, subject, skipSend):
+    msg = MIMEMultipart()
+    msg['From'] = fromAddr
+    msg['To'] = toAddrs
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+    text = msg.as_string()
+   
     try:  
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
         server.login(fromAddr, key)
         if(skipSend):
-		    print("Sending email from " + fromAddr + " to " + toAddrs + " Text: " + text);
+            print(text);
         else:
             server.sendmail(fromAddr, toAddrs, text)		
         server.close()
         return True
     except:  
+        print ("Unexpected error:", sys.exc_info()[0])
         return False
 		
--- temprature_monitor/temprature_monitor.py
+++ temprature_monitor/temprature_monitor.py
@@ -1,21 +1,45 @@
 from raspberry.gmail_utils import sendEmail
-import ConfigParser
+import configparser
+import time
 
-config = ConfigParser.ConfigParser()
+config = configparser.ConfigParser()
 config.read('env/config.cfg')
 gmailFromAddr = config.get('gmail', 'fromAddrs')
 gmailPassword= config.get('gmail', 'password')
 gmailToAddr = config.get('gmail', 'toAddrs')
 appMode = config.get('env', 'mode')
 
+cpuThreshold = config.getint('TEMPERATURE_MONITOR', 'cpuThreshold')
+gpuThreshold = config.getint('TEMPERATURE_MONITOR', 'gpuThreshold')
+checkInterval = config.getint('TEMPERATURE_MONITOR', 'checkInterval')
 
-def sendTemprature(temprature):
-    return sendEmail(gmailFromAddr, gmailToAddr, gmailPassword , "Hello, Please note , the temprature of your Raspberry is " + \
-    str( temprature), appMode == "dev")
+def getCPUTemperature():
+    return 60
+	
+def getGPUTemperature():
+    return 60
+
+
+
+def sendTemprature(cpuTemp, gpuTemp):
+    text = "Hello,\n\nCPU temperature is " \
+    + str( cpuTemp) + "\nGPU temperature is " +  str(gpuTemp)
+    return sendEmail(gmailFromAddr, gmailToAddr, gmailPassword , text , "Raspberry PI Temperature Alert",  appMode == "dev")
    
 
-sent = sendTemprature(85);
-msg =  "Email was sent" if sent else "Failed to sent email"	
-print(msg)
+   
+def checkTemperature():
+    cpuTemp = getCPUTemperature()
+    gpuTemp = getGPUTemperature()
+    print("CPU temperature: " + str(cpuTemp), ", GPU temperature: " + str(gpuTemp))
+    if(cpuTemp >= cpuThreshold or gpuTemp >= gpuThreshold) :
+        sent = sendTemprature(cpuTemp, gpuTemp);
+        msg =  "Email was sent" if sent else "Failed to sent email"	
+        print(msg)
+    else:
+        pass 
 	
-	
+while True:
+    checkTemperature()
+    time.sleep(checkInterval)   
+		
