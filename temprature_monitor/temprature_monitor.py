698a852bed95470ae1d3ab7f6c4c002bd1fdd08d
Sergey F <sergfa@gmail.com>
2016-09-30 13:49:33 +0300

    enable logs

--- temprature_monitor/temprature_monitor.py
+++ temprature_monitor/temprature_monitor.py
@@ -4,13 +4,17 @@ from raspberry.gmail_utils import sendEmail
 from raspberry.temperature_utils import getCpuTemperature
 from raspberry.temperature_utils import getGpuTemperature
 import os
-
 import configparser
 import time
+import logging
+
 
 config = configparser.ConfigParser()
-#config.read('env/config.cfg')
 config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../env', 'config.cfg'))
+
+logFileName =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '../logs', 'temperature.log')
+loggingLevel =  config.get('env', 'loggingLevel')
+logging.basicConfig(filename=logFileName,level=loggingLevel, format='%(asctime)s %(message)s')
 
 gmailFromAddr = config.get('gmail', 'fromAddrs')
 gmailPassword= config.get('gmail', 'password')
@@ -20,6 +24,8 @@ appMode = config.get('env', 'mode')
 cpuThreshold = config.getint('TEMPERATURE_MONITOR', 'cpuThreshold')
 gpuThreshold = config.getint('TEMPERATURE_MONITOR', 'gpuThreshold')
 checkInterval = config.getint('TEMPERATURE_MONITOR', 'checkInterval')
+
+logging.debug("Temperature monitor module has been started")
 
 def getCpuTemp():
     temp = 60 if appMode == "dev" else getCpuTemperature()
@@ -41,15 +47,20 @@ def sendTemprature(cpuTemp, gpuTemp):
 def checkTemperature():
     cpuTemp = getCpuTemp()
     gpuTemp = getGpuTemp()
-    print("CPU temperature: " + str(cpuTemp), ", GPU temperature: " + str(gpuTemp))
+    logging.debug("Current CPU temperature: " + str(cpuTemp) + ", GPU temperature: " + str(gpuTemp))
     if(cpuTemp >= cpuThreshold or gpuTemp >= gpuThreshold) :
         sent = sendTemprature(cpuTemp, gpuTemp);
         msg =  "Email was sent" if sent else "Failed to sent email"	
-        print(msg)
+        logging.debug(msg)
     else:
         pass 
-	
-while True:
-    checkTemperature()
-    time.sleep(checkInterval)   
+def main():	
+    while True:
+        checkTemperature()
+        time.sleep(checkInterval)
+
+try:
+    main()
+except:
+    logging.critical("Unexpected error:", sys.exc_info()[0])
 		
